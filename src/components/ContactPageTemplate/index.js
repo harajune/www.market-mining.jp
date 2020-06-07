0c2c40121e371a25a9442a59bb39b6b0cc132f5e
harajune <harajune@gijutsuya.jp>
2020-06-08 08:51:07 +0900

    add google form to contact page

--- src/components/ContactPageTemplate/index.js
+++ src/components/ContactPageTemplate/index.js
@@ -27,7 +27,11 @@ const ContactPageTemplate = ({ title, subtitle, meta_title, meta_description }) 
     </section>
     <section className='section'>
       <div className='container'>
-        <ContactForm />
+        <div className='columns'>
+          <div className='column is-10 is-offset-1'>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfpCPROpo1jeHLO-_2k61fTW8RN-pFblPRUINRAxrpVMs6KnA/viewform?embedded=true" width="640" height="1279" frameborder="0" marginheight="0" marginwidth="0" className='contact-frame'>読み込んでいます…</iframe>
+          </div>
+        </div>
       </div>
     </section>
   </div>
