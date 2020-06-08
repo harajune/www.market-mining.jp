97f4db89c4118aedb2dd88650d38ce06910b8f14
harajune <harajune@gijutsuya.jp>
2020-06-08 10:13:09 +0900

    cleanup content

--- config.js
+++ config.js
@@ -1,18 +1,12 @@
 module.exports = {
   siteTitle: 'Market Mining, Inc.', // Site title.
-  siteTitleAlt: 'Business', // Alternative site title for SEO.
-  siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
+  siteTitleAlt: 'マーケット・マイニング株式会社', // Alternative site title for SEO.
+  siteUrl: 'https://www.market-mining.tech/', // Domain of your website without pathPrefix.
   pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
-  siteDescription: 'Leverage Gatsby Business Starter for your Business.', // Website description used for RSS feeds/meta description tag.
+  siteDescription: 'マーケット・マイニング株式会社はマーケットを構造化しテクノロジーの進化を取り入れ変革することをMissinoとした、デジタル・トランスフォーメーション DX カンパニーです。', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
   siteFBAppID: '', // FB Application ID for using app insights
   googleTagManagerID: '', // GTM tracking ID.
-  disqusShortname: 'gatsby-business-starter', // Disqus shortname.
-  userName: 'Vaibhav Sharma',
-  userTwitter: 'vaibhaved',
-  userLocation: 'Delhi NCR, India',
-  userDescription: '',
   copyright: 'Copyright © Market Mining, Inc. 2018-2020. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
   themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
   backgroundColor: '#ffffff', // Used for setting manifest background color.
--- src/pages/contact/index.md
+++
@@ -1,8 +0,0 @@
----
-templateKey: 'contact-page'
-title: お問合せ
-subtitle: ご相談・お問い合わせはこちらのフォームからお願いします。
-meta_title: お問合せ | Market Mining, Inc
-meta_description: >-
-  デジタル・トランスフォーメーションDXやITを活用した新しいUX・生産性の向上の実現と支援をにご興味がある方は、下記のフォームからマーケット・マイニング株式会社へお問合せください。
----
